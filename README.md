ce568f2663d9e822ffcafe1468a7ad2debd188c9
Loïc MARIE <loic.marie54@gmail.com>
2015-09-20 17:10:00 +0200

    Adding pieces files and firsts tests

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # chessmatic-board
 Chessboard is an internal board representation to maintain chess positions for search, evaluation and game-play. It's a NodeJS module.
-
-## Features
-* Manage a game
-  * Create a new game
-  * Play/Undo a move
-  * Get historic of positions
-  * Moves validation
-* Make analysis
-  * Get position in the expected format
-  * Get allowed moves for a position
--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-"use strict";
+module.exports = {
+	run: function(){
+		var Chessboard = require('./lib/Chessboard.js'),
+			chessboard = new Chessboard();
 
-var Piece = require('./lib/Pieces/Piece.js'),
-	piece = new Piece('jean');
-
-piece.say();
+		console.log(chessboard.show());
+		chessboard.play('Nf3');
+		console.log(chessboard.show());
+		chessboard.play('d6');
+		console.log(chessboard.show());
+		chessboard.play('Nc3');
+		console.log(chessboard.show());
+		chessboard.play('e5');
+		console.log(chessboard.show());
+		chessboard.play('d4');
+		console.log(chessboard.show());
+		chessboard.play('exd4');
+		console.log(chessboard.show());
+		chessboard.play('Nxd4');
+		console.log(chessboard.show());
+	}
+}
